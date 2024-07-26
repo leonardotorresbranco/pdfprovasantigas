87e8b2ad896427dc1e9ce441a1f09a6f86a0a34c
Leonardo Branco <leonardotorresbranco@gmail.com>
2024-07-26 10:18:57 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from pdfreader import SimplePDFViewer
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/extract-last-page-text', methods=['POST'])
+def extract_last_page_text():
+    pdf_file = request.files['file']
+    pdf_bytes = pdf_file.read()
+
+    viewer = SimplePDFViewer(BytesIO(pdf_bytes))
+    last_page_num = len(viewer.pages)
+    viewer.navigate(last_page_num)
+    
+    viewer.render()
+    text = "".join(viewer.canvas.strings)
+
+    return jsonify({"text": text})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
