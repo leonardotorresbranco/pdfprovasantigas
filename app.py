6a3d5d03d8d01e11983e224659f94e73e100b385
Leonardo Branco <leonardotorresbranco@gmail.com>
2024-07-26 20:37:36 -0300

    pdf

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import base64
-import io  # Import the io module
-import PyPDF2
+import io
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 CORS(app)
@@ -16,11 +16,11 @@ def extract_last_page_text():
 
         pdf_data = base64.b64decode(data['file'])
         
-        pdf_reader = PyPDF2.PdfFileReader(io.BytesIO(pdf_data))
-        num_pages = pdf_reader.getNumPages()
+        pdf_reader = PdfReader(io.BytesIO(pdf_data))
+        num_pages = len(pdf_reader.pages)
 
         # Extract text from the last page
-        last_page = pdf_reader.getPage(num_pages - 1)
+        last_page = pdf_reader.pages[num_pages - 1]
         last_page_text = last_page.extract_text()
         
         return jsonify({'text': last_page_text})
@@ -37,13 +37,13 @@ def extract_questions_text():
 
         pdf_data = base64.b64decode(data['file'])
         
-        pdf_reader = PyPDF2.PdfFileReader(io.BytesIO(pdf_data))
-        num_pages = pdf_reader.getNumPages()
+        pdf_reader = PdfReader(io.BytesIO(pdf_data))
+        num_pages = len(pdf_reader.pages)
 
         # Extract text from all pages except the last one
         questions_text = ""
         for page_num in range(num_pages - 1):
-            page = pdf_reader.getPage(page_num)
+            page = pdf_reader.pages[page_num]
             questions_text += page.extract_text() + "\n"
         
         return jsonify({'text': questions_text.strip()})
